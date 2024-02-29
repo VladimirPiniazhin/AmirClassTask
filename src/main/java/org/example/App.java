e3af56562d55bbe67ea14a461b1aedf02619f70b
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2024-02-29 14:32:33 +0200

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static int addMethod(int a, int b) {
+        return a + b;
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!"+addMethod(2,3) );
+    }
+}
